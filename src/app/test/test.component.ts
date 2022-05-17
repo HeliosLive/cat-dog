4d09db36934651c7dbab0896825eff2e398242cd
= <=>
2022-05-17 16:10:11 +0200

    feature(eslint): ESlint added into the project connected to prettier

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss'],
 })
-export class TestComponent implements OnInit {
+export class TestComponent {
   liked = false;
-
-  constructor() {}
-
-  ngOnInit() {}
 
   onLike(): void {
     this.liked = !this.liked;
